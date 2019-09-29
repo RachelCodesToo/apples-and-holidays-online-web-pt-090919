6fa13e0b01c9bf6f820988baf9f2da8f67497d31
Anne Fausto <a.r.fausto@gmail.com>
2019-09-29 02:04:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   cookout = []
   holiday_hash.each {|season, celebration|
-    holiday.each {|holiday, supply|
+    holi.each {|holiday, supply|
        cookout << holiday if supply.include?("BBQ")
     }
   } 
